7f59de990786581e946c037d62b73cedfb1f60db
0xlvl3 <cryptochoccy@gmail.com>
2023-06-23 20:15:41 +1000

    testing flask

--- social-network/main.py
+++ social-network/main.py
@@ -1,1 +1,17 @@
-print("test")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Welcome to the social network"
+
+
+@app.route("/test")
+def test():
+    return render_template("test.html", title="Social network", heading="Test heading")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
